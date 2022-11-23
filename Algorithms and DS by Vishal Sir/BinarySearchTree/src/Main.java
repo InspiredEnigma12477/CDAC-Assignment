0b38ad45752ec59495f2276b757cb429c4eff72e
InspiredEnigma12477 <sakoreshivam3901@gmail.com>
2022-11-23 10:17:29 +0530

    aptitude added and tree

---
+++ Algorithms and DS by Vishal Sir/BinarySearchTree/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+
+        bst.insert(50);
+        bst.insert(30);
+        bst.insert(80);
+        bst.insert(20);
+        bst.insert(40);
+        bst.insert(35);
+        bst.insert(60);
+        bst.insert(100);
+        bst.insert(70);
+        System.out.println( bst.insert(70) );
+
+        bst.preOrder( bst.getRoot() );
+        System.out.println();
+        bst.preOrder();
+        System.out.println();
+
+        bst.inOrder( bst.getRoot() );
+        System.out.println();
+        bst.inOrder();
+        System.out.println();
+
+
+        bst.postOrder( bst.getRoot() );
+        System.out.println();
+        bst.postOrder();
+        System.out.println();
+
+        System.out.println("Max = " + bst.getMax() );
+        System.out.println("Min = " + bst.getMin() );
+    }
+}
+
